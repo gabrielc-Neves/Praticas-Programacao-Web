0f35f60e48917a80e4296a95e2870e1ffa71dfd6
gabrielc-Neves <gabrielcneves17@gmail.com>
2025-09-10 08:56:06 -0300

    Adicionando arquivos

---
+++ curso de react/src/components/AddTask.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+function Addtask({ onAddtaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+      <input
+        type="text"
+        placeholder="Digite o titulo da tarefa "
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Digite a descricao da tarefa tarefa "
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => onAddTaskSubmit(title, description)}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+export default Addtask;
